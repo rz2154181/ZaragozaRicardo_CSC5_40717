684655f4e296530cc693191e5bbb1eafaf4b8695
rz2154181 <fukvtec@yahoo.com>
2015-01-07 11:30:54 -0800

    Signed-off-by: rz2154181 <fukvtec@yahoo.com>

---
+++ Class_Lab/MathInstructorDisagreement/main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   main.cpp
+ * Author: Ricardo Zaragoza
+ * Created on January 7, 2015, 11:17 AM
+ * Purpose: How easy computers can make mistakes
+ */
+
+//System Libraries
+#include <iostream>
+
+//User Libraries
+using namespace std;
+
+//Global Functions
+
+//Function Prototypes
+
+//Execution begins here!
+int main(int argc, char** argv) {
+    //Declare 3 Variables
+    short a, b, c;
+    //Prompt for a and b
+    cout<<"Input 2 short values"<<endl;
+    cin>>a>>b;
+    //Sum the values
+    c=a+b;
+    //Output the values
+    cout<<c<<" = "<<a<<" + "<<b<<endl;
+    //Exit stage right!
+    return 0;
+}
+
